0c1d7423bbc9441df9e346f2143b8c9ff03128cf
Surender Lohia <surenloh@thoughtworks.com>
2020-03-26 21:11:35 +0530

    Add queue

---
+++ src/queue.js
@@ -0,0 +1,113 @@
+function Queue() {
+  let collection = [];
+
+  this.print = function() {
+    console.log(collection);
+  }
+
+  this.enQueue = function(item) {
+    return collection.push(item);
+  }
+
+  this.deQueue = function() {
+    return collection.shift();
+  }
+
+  this.front = function() {
+    return collection[0];
+  }
+
+  this.size = function() {
+    return collection.length;
+  }
+
+  this.isEmpty = function() {
+    return collection.length === 0;
+  }
+
+  this.values = function() {
+    return collection;
+  }
+}
+
+function priorityQueue() {
+  let collection = [];
+  this.print = function() {
+    console.log('collection');
+  }
+
+  this.enQueue = function(element) {
+    if(collection.length === 0) {
+      collection.push(element);
+    } else {
+      var added = false;
+      for(let i = 0; i < collection.length; i++) {
+        // checking priority
+        if(element[1] < collection[i][1]) {
+          collection.splice(i, 0, element);
+
+          added = true;
+          break;
+        }
+      }
+
+      if(!added) {
+        collection.push(element);
+      }
+    }
+  }
+
+  this.deQueue = function() {
+    var value = collection.shift();
+    return value[0];
+  };
+
+  this.front = function() {
+    return collection[0];
+  }
+
+  this.size = function() {
+    return collection.length;
+  }
+
+  this.isEmpty = function() {
+    return collection.length === 0;
+  }
+}
+
+// // Test cases
+// let q = Queue();
+// q.enQueue('a');
+// q.enQueue('b');
+// q.enQueue('b');
+// q.print(); // ['a', 'b', 'c'];
+
+// q.deQueue();
+// q.print(); // ['b', 'c']
+
+// q.front(); // b
+// q.size(); // 2
+// q.isEmpty(); // false
+
+// // Test for priorityQueue
+// var pq = new priorityQueue();
+
+// pq.enQueue(['CSS', 2]);
+// pq.enQueue(['HTML', 3]);
+// pq.enQueue(['JavaScript', 1]);
+
+// pq.print(); // [['JavaScript', 1], ['CSS', 2], ['HTML', 3]]
+
+// pq.deQueue();
+// pq.print() // [['CSS', 2], ['HTML', 3]]
+
+// console.log(pq.front()); // ['CSS', 2]
+
+// console.log(pq.size()); // 2
+
+
+module.exports = {
+  Queue,
+  priorityQueue
+};
+
---
+++ test/queue.js
@@ -0,0 +1,33 @@
+var Queue = require('./../src/queue').Queue;
+var expect = require('chai').expect;
+
+
+describe('Queue', function() {
+  let q;
+
+  this.beforeEach(function() {
+    // Basic setup
+    q = new Queue();
+    
+    q.enQueue('a');
+    q.enQueue('b');
+    q.enQueue('c');
+  });
+
+  it('#enQueue should add a given element in to the queue', function() {
+    const expectedSet = ['a', 'b', 'c', 'd'];
+
+    q.enQueue('d')
+    expect(q.values()).to.deep.equal(expectedSet);
+  });
+
+  it.only('#deQueue should remove element from the queue', function() {
+    const expectedSet = ['b', 'c'];
+
+    q.deQueue();
+    expect(q.values()).to.deep.equal(expectedSet);
+  });
+
+  
+  // Todo: Need to add test cases for other methods
+});
